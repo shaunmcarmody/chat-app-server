c00659d048590cb8d37c5f82e4cb5a234647f89b
shaunmcarmody <shaun.carmody@live.co.uk>
2020-01-14 21:50:42 +0000

    init chat classes

--- api/index.js
+++ api/index.js
@@ -2,4 +2,29 @@ const express = require('express');
 
 const app = express();
 
+class Chat {
+  constructor(participants = [], messages = [], lastMessage = null) {
+    this.participants = participants;
+    this.messages = messages;
+    this.lastMessage = lastMessage;
+  }
+}
+
+class Message {
+  constructor(id, person, message) {
+    this.id = id;
+    this.person = person;
+    this.message = message;
+  }
+}
+
+class Person {
+  constructor(id, name) {
+    this.id = id;
+    this.name = name;
+  }
+}
+
+const chat = new Chat();
+
 module.exports = app;
